7134bda17c9cb0fe36a256cb5b7a2683387f1fc8
dhanush <dhanushvenkatesan.in@gmail.com>
2025-10-29 20:04:47 +0530

    push

---
+++ Day-17/Task7.js
@@ -0,0 +1,21 @@
+let a="str";
+let b=22;
+let c=[1,2,3,4];
+let d = { 
+    x:'strin',
+    y:33,
+    z:'true'  };
+
+let e;
+let f=null;
+let g=true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+
---
+++ Day-17/Task8.js
@@ -0,0 +1,8 @@
+let Movie=["f1","dude","kanthara2"];
+const pc={
+    Brand:"victus",
+    RAM:"20",
+    Price:55000
+};
+console.log("Favorite Movie:",Movie);
+console.log("Laptop:",pc);
